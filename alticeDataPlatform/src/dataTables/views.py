69bb3b39012af8ec2c4adad27fa483867a948965
Shahran Islam <ssi256@nyu.edu>
2018-11-06 11:06:49 -0500

    edits

--- alticeDataPlatform/src/dataTables/views.py
+++ alticeDataPlatform/src/dataTables/views.py
@@ -5,7 +5,19 @@ from django.views.generic import TemplateView
 
 
 
-class homePage(TemplateView):
+class homepage(TemplateView):
     template_name = "MainLandingPage.html" 
+
+class dashboardCurrent(TemplateView):
+    template_name = "CurrentCustomerDashBoard.html" 
+
+class dashboardNew(TemplateView):
+    template_name = "NewAccquiredCustomerDashBoard.html" 
+
+class dashboardProspective(TemplateView):
+    template_name = "ProspectiveCustomerDashBoard.html" 
+
+class CustomerProfile(TemplateView):
+    template_name = "Customerprofile.html"
 
 # Create your views here.
