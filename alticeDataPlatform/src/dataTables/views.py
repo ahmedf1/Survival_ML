6ec2fc09e8879943f9f42522a1a4426ccf2f2aeb
Redwanul (Red) Mutee <rm4243@nyu.edu>
2018-11-06 08:28:05 -0500

    urls.py error needs fix

--- alticeDataPlatform/src/dataTables/views.py
+++ alticeDataPlatform/src/dataTables/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+
+
+class homePage(View):
+    def get(self,request, *arg, **kwargs):
+        context = {}
+        return render(request,"template/MainLandingPage.html",context) 
 
 # Create your views here.
