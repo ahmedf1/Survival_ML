8254d9c443650e791c434e2f8271db91baf74a7d
Farhad Ahmed <fa961@nyu.edu>
2018-11-06 00:07:00 -0500

    added machine learning python script

---
+++ multilinearRegression.py
@@ -0,0 +1,16 @@
+
+
+# importing some libraries to do the math and graphing
+import numpy as np
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+
+
+# load the data set using the python pandas library
+
+#first define the names of the columns
+
+col_names = [
+    'Customer Name', 'Location', 'Age', 'Scaled Age', 'Credit Score', 'Scaled Credit Score', 'Income(Thousands)', 'Scaled Income', 'Household Size', 'Scaled Household', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+    ]
